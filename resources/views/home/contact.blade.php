177afe08a3401a47179e015e5457881743e6ecb5
virganda <133211416+Virganda@users.noreply.github.com>
2024-07-01 07:55:26 +0700

    Update contact.blade.php

--- resources/views/home/contact.blade.php
+++ resources/views/home/contact.blade.php
@@ -34,8 +34,8 @@
           </div>
           <div class="col-md-6">
              <div class="map_main">
-                <div class="map-responsive">
-                   <iframe src="https://www.google.com/maps/embed/v1/place?key=AIzaSyA0s1a7phLN0iaD6-UE7m4qP-z21pH0eSc&amp;q=Eiffel+Tower+Paris+France" width="600" height="400" frameborder="0" style="border:0; width: 100%;" allowfullscreen=""></iframe>
+               <div class="map-responsive">
+                   <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3950.028670539372!2d112.18139267514667!3d-8.098558391930213!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e78ec64baf88d45%3A0xf356d9dd9e6b3a31!2sUniversitas%20Islam%20Balitar%20-%20UNISBA%20BLITAR!5e0!3m2!1sid!2sid!4v1719795090094!5m2!1sid!2sid" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></src=>" width="600" height="400" frameborder="0" style="border:0; width: 100%;" allowfullscreen=""></iframe>
                 </div>
              </div>
           </div>
